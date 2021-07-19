4ba636af6515366b54bcd29c1d0145766da6e466
Zhao Liang <gamer.akira@gmail.com>
2021-07-20 00:52:16 +0800

    TwoSum

---
+++ src/main/java/com/akira/leetcode/editor/cn/TwoSum.java
@@ -0,0 +1,76 @@
+//Given an array of integers nums and an integer target, return indices of the t
+//wo numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 104 
+// -109 <= nums[i] <= 109 
+// -109 <= target <= 109 
+// Only one valid answer exists. 
+// 
+//
+// 
+//Follow-up: Can you come up with an algorithm that is less than O(n2) time comp
+//lexity? Related Topics 数组 哈希表 
+// 👍 11618 👎 0
+
+
+package com.akira.leetcode.editor.cn;
+
+/**
+ * @author Zhao Liang
+ * 2021-07-19 23:48:08
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new TwoSum().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            int remain = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == remain) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
